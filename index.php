a73ff07dc2cd4c99daad6d8c1fa19439935512a7
AnthonySk <anthony.statek@gmail.com>
2024-02-05 22:27:29 +0100

    add files for header.html

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<!------- START BODY ------->
+
+<body>
+
+    <!------- START HEADER ------->
+<?php include_once "header.php" ?>
+    <!------- END HEADER ------->
+
+
+
+
+    <!------- START MAIN ------->
+    <main>
+    
+    </main>
+    <!------- END MAIN ------->
+
+
+
+
+    <!------- START FOOTER ------->
+    <footer>
+
+    </footer>
+    <!------- END FOOTER ------->
+
+
+
+</body>
+
+<!------- END BODY ------->
+
+
+</html>
