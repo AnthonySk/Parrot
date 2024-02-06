4d2c181d4b22dbc53c24c103a4edac44e4d48dbd
AnthonySk <anthony.statek@gmail.com>
2024-02-06 13:18:14 +0100

    index.php suivi

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+
+<!------- START BODY ------->
+
+<body>
+
+    <!------- START HEADER ------->
+<?php require_once "header.html" ?>
+
+    <!------- END HEADER ------->
+
+
+
+
+    <!------- START MAIN ------->
+    <main>
+        <div class="imgCover"></div>
+        <h2 class="p-3 d-flex justify-content-center">Nos services</h2>
+        <div class="container">
+            <div class="p-3 row  gap-1">
+                <div class="col">
+                    <div class="card">
+                        <img class ="card-img-top img-fluid" src="img/services vidange.jpg" alt="image vidange">
+                        <div class="card-body p-1">
+                            <h3 class="card-title">Vidange</h3>
+                            <p class="card-text">Nous changeons les filtres de votre véhicules (air, habitacle et huile)</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img class ="card-img-top img-fluid" src="img/services vidange.jpg" alt="image vidange">
+                        <div class="card-body p-1">
+                            <h3 class="card-title">Vidange</h3>
+                            <p class="card-text">Nous changeons les filtres de votre véhicules (air, habitacle et huile)</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img class ="card-img-top img-fluid" src="img/services vidange.jpg" alt="image vidange">
+                        <div class="card-body p-1">
+                            <h3 class="card-title">Vidange</h3>
+                            <p class="card-text">Nous changeons les filtres de votre véhicules (air, habitacle et huile)</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img class ="card-img-top img-fluid" src="img/services vidange.jpg" alt="image vidange">
+                        <div class="card-body p-1">
+                            <h3 class="card-title">Vidange</h3>
+                            <p class="card-text">Nous changeons les filtres de votre véhicules (air, habitacle et huile)</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!------- END MAIN ------->
+
+
+
+
+    <!------- START FOOTER ------->
+    <footer>
+
+    </footer>
+    <!------- END FOOTER ------->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+<!------- END BODY ------->
+
+
+</html>
